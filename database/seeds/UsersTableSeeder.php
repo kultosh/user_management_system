a254c6f59d005f27bc55f0066906e9129d042792
kultosh <kulgole90@gmail.com>
2020-06-15 19:01:36 +0545

    Created roles and relationshp with the user model

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\User;
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      User::truncate();
+
+      $adminRole = Role::where('name','admin')->first();
+      $authorRole = Role::where('name','author')->first();
+      $userRole = Role::where('name','user')->first();
+
+      $admin = User::create([
+        'name' => 'admin',
+        'email' => 'admin@admin.com',
+        'password' => bcrypt('secretadmin'),
+      ]);
+
+      $author = User::create([
+        'name' => 'author',
+        'email' => 'author@author.com',
+        'password' => bcrypt('secretauthor'),
+      ]);
+
+      $user = User::create([
+        'name' => 'user',
+        'email' => 'user@user.com',
+        'password' => bcrypt('secretuser'),
+      ]);
+
+      $admin->roles()->attach($adminRole);
+      $author->roles()->attach($authorRole);
+      $user->roles()->attach($userRole);
+    }
+}
