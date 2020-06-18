4bab576336bd8a76f1c6a2437178e76cb9a682d2
kultosh <kulgole90@gmail.com>
2020-06-18 20:00:57 +0545

    Hide the UI Elements as per the role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,9 +33,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                      <li class="nav-item">
-                        <a href="{{route('admin.users.index')}}">Manage Users</a>
-                      </li>
+                      @hasrole('admin')
+                         <li class="nav-item">
+                          <a href="{{route('admin.users.index')}}">Manage Users</a>
+                        </li>
+                      @endhasrole
                     </ul>
 
                     <!-- Right Side Of Navbar -->
