7a90cf4153ca25c5b661b7fe94f25664dc8dde9e
kultosh <kulgole90@gmail.com>
2020-06-18 23:57:34 +0545

    Deleted the foreign key role_user tables on delete of the role of the user by the admin

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -68,7 +68,16 @@ class UserController extends Controller
           return redirect()->route('admin.users.index')->with('warning', 'You are not allowed to delete yourself!');
         }
 
-        User::destroy($id);
-        return redirect()->route('admin.users.index')->with('success', 'User has been deleted successfully!');
+        $user = User::find($id);
+
+        if($user)
+        {
+          $user->roles()->detach();
+          $user->delete();
+          return redirect()->route('admin.users.index')->with('success', 'User has been deleted');
+        }
+
+        return redirect()->route('admin.users.index')->with('warning', 'This user can not be delete');
+
     }
 }
